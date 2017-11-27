6d6690978f2745f7be7e6cde5244a418fbb03ff6
BabluKumar17 <hacback17@gmail.com>
2017-11-27 12:14:39 +0530

    js speechRec

--- sketch.js
+++ sketch.js
@@ -20,23 +20,23 @@ function voiceReady(){
 
 
 ///////////////////////////
-// let lang = navigator.language || 'en-US';
-// let speechRec = new p5.SpeechRec(lang, gotSpeech);
-// let continuous = true;
-// let interim = false;
-// speechRec.start(continuous, interim);
+let lang = navigator.language || 'en-US';
+let speechRec = new p5.SpeechRec(lang, gotSpeech);
+let continuous = true;
+let interim = false;
+speechRec.start(continuous, interim);
 
-// function gotSpeech(){
-// 	if (speechRec.resultValue){
-// 		let input = speechRec.resultString;
-// 		console.log(input);
-// 		user_input.value(input);
-// 		reply = bot.reply("local-user", input);
-// 		output.html(reply);
+function gotSpeech(){
+	if (speechRec.resultValue){
+		let input = speechRec.resultString;
+		console.log(input);
+		user_input.value(input);
+		reply = bot.reply("local-user", input);
+		output.html(reply);
 		
-// 	}
-// 	// console.log(speechRec);
-// }
+	}
+	// console.log(speechRec);
+}
 //////////////////////////
 
 function brainReady() {
